87106c2f19b0b52a576d189d1c85805d1e68dfd8
arielliv <ariell@wix.com>
2024-03-21 20:53:41 +0200

    [infrastructure]: added infrastructure for jest and react testing lib, with one unit test and one e2e test for example

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-
+import type {Config} from 'jest';
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
@@ -11,7 +11,7 @@ const createJestConfig = nextJest({
     dir: './',
 })
 
-const config = {
+const config: Config = {
     // All imported modules in your tests should be mocked automatically
     // automock: false,
 
