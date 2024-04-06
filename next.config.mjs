ab554ad43d2e9214534a628f73e6bdc315075dea
arielliv <ariell@wix.com>
2024-04-06 22:06:09 +0300

    [infra]: fix docker build

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
+  webpack: (config, { _isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
