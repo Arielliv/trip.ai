7c354f605e69c7435b72a555bba37bc614cf5ee0
AdiVeiszman <adiveiszman@gmail.com>
2024-05-07 23:15:54 +0300

    [location edit] editLocation, isEditMode are now from useLocationContext

--- src/app/hooks/useOnFormSubmit.tsx
+++ src/app/hooks/useOnFormSubmit.tsx
@@ -3,13 +3,11 @@ import { createLocation, updateLocation } from '@/lib/data';
 import { SubmitHandler } from 'react-hook-form';
 import { ILocation } from '@/models/Location';
 import { mapLocationFormDataToLocationSchema } from '@/models/mappers/mapLocationFormDataToLocationSchema';
+import { useLocationContext } from '@/app/providers/LocationContextFormProvider/LocationContextFormProvider';
 
-export const useOnFormSubmit = (
-  addLocation: (newLocation: ILocation) => void,
-  editLocation: (updatedLocation: ILocation) => void,
-  isEditMode: boolean,
-) => {
+export const useOnFormSubmit = (addLocation: (newLocation: ILocation) => void) => {
   const { formState } = useLocationForm();
+  const { editLocation, isEditMode } = useLocationContext();
 
   const onSubmit: SubmitHandler<LocationFormData> = async (data): Promise<void> => {
     if (Object.keys(formState.errors).length === 0) {
