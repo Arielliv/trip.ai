42ece2c1e40a2cc8d844ffd016cd9c30b7db26fe
Tal Koren <talko4245@gmail.com>
2024-05-09 18:10:08 +0300

    Create and update db and clint object to work with new premissions objects
    Signed-off-by: Tal Koren <talko4245@gmail.com>

--- src/models/Location.ts
+++ src/models/Location.ts
@@ -1,7 +1,8 @@
 /* v8 ignore start */
 import { Schema, Types, models, model } from 'mongoose';
 import { LocationType, Role, Visibility } from '@/models/constants';
-import { Address, Coordinate, Permission } from '@/models/shared/types';
+import { Address, Coordinate } from '@/models/shared/types';
+import { BLPermissions, permissionScheme } from '@/models/businessLogicPermissions';
 
 export interface ILocation {
   _id?: string;
@@ -16,7 +17,7 @@ export interface ILocation {
   coordinates: Coordinate;
   address: Address;
   visibility: Visibility;
-  permissions: Permission[];
+  permissions: BLPermissions;
   mapsUrl?: string;
   links?: string[];
   businessStatus?: string;
@@ -36,7 +37,7 @@ export interface ILocationDto {
   address: Address;
   visibility: Visibility;
   user_id: Types.ObjectId;
-  permissions: Permission[];
+  permissions: BLPermissions;
   mapsUrl?: string; // URL to the Google Maps page
   links?: string[]; // New: Array of additional links, e.g., the website
   businessStatus?: string; // New: Optional business status
@@ -54,27 +55,26 @@ const LocationSchema: Schema = new Schema({
   placeTypes: [{ type: String }],
   coordinates: {
     latitude: { type: Number, required: true },
-    longitude: { type: Number, required: true },
+    longitude: { type: Number, required: true }
   },
   address: {
     street: { type: String },
     city: { type: String },
     state: { type: String },
     country: { type: String },
-    postalCode: { type: String },
+    postalCode: { type: String }
   },
   visibility: { type: String, enum: Object.values(Visibility), required: true },
   user_id: { type: Types.ObjectId, required: true },
-  permissions: [
-    {
-      user_id: { type: Types.ObjectId, required: true, ref: 'User' },
-      role: { type: String, enum: Object.values(Role), required: true },
-    },
-  ],
+  permissions: {
+    type: permissionScheme,
+    required: true,
+    ref: 'Permissions'
+  },
   mapsUrl: { type: String },
   links: [{ type: String }], // Added
   businessStatus: { type: String }, // Added as optional
-  imageUrl: { type: String },
+  imageUrl: { type: String }
 });
 
 const Location = models.Location || model<ILocation>('Location', LocationSchema);
--- src/models/Trip.ts
+++ src/models/Trip.ts
@@ -2,6 +2,7 @@
 import { Schema, Document, Types, models, model } from 'mongoose';
 import { LocationType, Role, TransportationType, Visibility } from '@/models/constants';
 import { Address, Coordinate, Permission } from '@/models/shared/types';
+import { BLPermissions, permissionScheme } from '@/models/businessLogicPermissions';
 
 export interface ITrip {
   _id?: string;
@@ -19,7 +20,7 @@ export interface ITripDto {
   owner: Types.ObjectId;
   name: string;
   participants_ids: Types.ObjectId[];
-  permissions: Permission[];
+  permissions: BLPermissions;
   locations: TripLocation[];
   locations_order: Types.ObjectId[];
   visibility: Visibility;
@@ -54,22 +55,20 @@ const TripLocationSchema: Schema = new Schema({
   location: { type: Types.ObjectId, ref: 'Location', required: true },
   time: {
     start: { type: Date, required: true },
-    end: { type: Date, required: true },
+    end: { type: Date, required: true }
   },
   note: { type: String },
-  ticket: { type: String },
+  ticket: { type: String }
 });
 
 const TripSchema: Schema = new Schema({
   name: { type: String, required: true },
   owner: { type: Types.ObjectId, required: true },
   participants_ids: [{ type: Types.ObjectId }],
-  permissions: [
-    {
-      user_id: { type: Types.ObjectId, required: true, ref: 'User' },
-      role: { type: String, enum: Object.values(Role), required: true },
-    },
-  ],
+  permissions: {
+    type: permissionScheme,
+    required: true
+  },
   locations: [TripLocationSchema],
   locations_order: [{ type: Types.ObjectId }],
   visibility: { type: String, enum: Object.values(Visibility), required: true },
@@ -77,17 +76,17 @@ const TripSchema: Schema = new Schema({
     {
       origin: { type: Types.ObjectId, required: true },
       destination: { type: Types.ObjectId, required: true },
-      type: { type: String, enum: Object.values(TransportationType), required: true },
-    },
+      type: { type: String, enum: Object.values(TransportationType), required: true }
+    }
   ],
   reviews: [
     {
       user_id: { type: Types.ObjectId, required: true },
       rate: { type: Number, required: true },
       description: { type: String, required: true },
-      images: [{ type: String }],
-    },
-  ],
+      images: [{ type: String }]
+    }
+  ]
 });
 
 const Trip = models.Trip || model<ITrip>('Trip', TripSchema);
