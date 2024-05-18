3990d2b58dea62bc80fa312d7e95572020fc54ef
arielliv <ariell@wix.com>
2024-05-18 12:22:51 +0300

    [trips]: changed trip dashboard UI

--- src/app/components/LocationsTableView/LocationsTableView.tsx
+++ src/app/components/LocationsTableView/LocationsTableView.tsx
@@ -14,10 +14,13 @@ export const LocationsTableView = () => {
         </Grid>
         <Grid xs={12} padding={1}>
           <Typography component={'p'} variant="body1">
-            Here you can arrange your trip by adding locations with extra information about them.
+            Here, you can plan your journey in detail. Start by adding each destination you wish to visit.
           </Typography>
           <Typography component={'p'} variant="body1">
-            It can be dates, time duration or even cost, try it to find out
+            For every location, you can specify dates, the duration of stay, and estimated costs.
+          </Typography>
+          <Typography component={'p'} variant="body1">
+            Customize your trip to match your schedule and budget. Explore the features below to enhance your itinerary!
           </Typography>
         </Grid>
         <Grid xs={12} sx={{ height: '700px' }}>
