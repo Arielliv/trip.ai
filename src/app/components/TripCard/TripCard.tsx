63210d4345142f2bb89b032b292b06aa3ef2aae7
arielliv <ariell@wix.com>
2024-05-27 19:54:59 +0300

    [explore]: mini trip view in explore page

--- src/app/components/TripCard/TripCard.tsx
+++ src/app/components/TripCard/TripCard.tsx
@@ -1,6 +1,7 @@
 import { Button, Card, CardContent, CardMedia, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { ITrip } from '@/models/Trip';
+import { TripMiniView } from '@/app/components/TripMiniView/TripMiniView';
 
 export interface TripCardProps {
   trip: ITrip;
@@ -8,21 +9,35 @@ export interface TripCardProps {
 }
 
 export const TripCard = ({ trip }: TripCardProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Card>
-      <CardMedia component="img" height="140" image={'/path-to-trip-image.jpg'} alt="Trip image" />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {trip.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Duration:
-        </Typography>
-        <Typography variant="body2">Locations: {trip.locations.length}</Typography>
-        <Button fullWidth variant="contained" sx={{ marginTop: 2 }}>
-          Check it out
-        </Button>
-      </CardContent>
-    </Card>
+    <>
+      <Card>
+        <CardMedia component="img" height="140" image={'/path-to-trip-image.jpg'} alt="Trip image" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {trip.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Duration:
+          </Typography>
+          <Typography variant="body2">Locations: {trip.locations.length}</Typography>
+          <Button fullWidth variant="contained" sx={{ marginTop: 2 }} onClick={handleOpen}>
+            Check it out
+          </Button>
+        </CardContent>
+      </Card>
+
+      <TripMiniView trip={trip} isOpen={isOpen} handleClose={handleClose} />
+    </>
   );
 };
