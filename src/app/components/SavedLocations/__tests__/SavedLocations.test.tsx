18d29f1740061e29247693ff83eef111d4e646f8
Talkoren611 <tal.koren@nice.com>
2024-06-06 18:23:23 +0300

    Refactor failing tests
    Signed-off-by: Talkoren611 <tal.koren@nice.com>

--- src/app/components/SavedLocations/__tests__/SavedLocations.test.tsx
+++ src/app/components/SavedLocations/__tests__/SavedLocations.test.tsx
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { SavedLocationsDriver } from '@/app/components/SavedLocations/__tests__/SavedLocations.driver';
+import { ObjectId } from 'mongodb';
+import { LocationPermissionEnum } from '@/models/enums/permissionsEnums';
 
 describe('SavedLocations', () => {
   let driver: SavedLocationsDriver;
@@ -11,8 +13,22 @@ describe('SavedLocations', () => {
 
   it('renders locations if present in the database', async () => {
     const mockLocations = [
-      { id: 1, name: 'Location 1', imageUrl: 'http://example.com/img1.jpg' },
-      { id: 2, name: 'Location 2', imageUrl: 'http://example.com/img2.jpg' },
+      {
+        id: 1,
+        name: 'Location 1',
+        imageUrl: 'http://example.com/img1.jpg',
+        permissions: [
+          { userId: new ObjectId('6621274fe078e5a753823333'), permissionType: LocationPermissionEnum.edit },
+        ],
+      },
+      {
+        id: 2,
+        name: 'Location 2',
+        imageUrl: 'http://example.com/img2.jpg',
+        permissions: [
+          { userId: new ObjectId('6621274fe078e5a7538250cd'), permissionType: LocationPermissionEnum.edit },
+        ],
+      },
     ];
 
     driver.givenFetchLocationMock({
